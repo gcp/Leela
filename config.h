9702ecc30fbac4102949b7f4ec61b19fad6e9c66
Gian-Carlo Pascutto <gcp@sjeng.org>
2016-11-28 21:38:33 +0100

    Update learning data.

--- config.h
+++ config.h
@@ -18,7 +18,7 @@
 
 #define PROGRAM_NAME "Leela"
 //#define PROGRAM_VERSION "0.8.0. I will resign when I am lost. If you are sure you are winning but I haven't resigned yet, the status of some groups is not yet clear to me. I will pass out the game when I am won. You can download the latest stable version at https://sjeng.org/leela"
-#define PROGRAM_VERSION "0.8.0 va3"
+#define PROGRAM_VERSION "0.8.0 va4"
 
 /* Features */
 #define USE_BLAS
