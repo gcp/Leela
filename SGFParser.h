d955a9c1e32b303a13cc37ec49cb101cd865bfa6
Gian-Carlo Pascutto <gcp@sjeng.org>
2017-01-09 20:12:26 +0100

    Make SGF parsers more robust, clean up.

--- SGFParser.h
+++ SGFParser.h
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>
+#include <climits>
 
 #include "SGFTree.h"
 
@@ -11,10 +12,11 @@ private:
     static std::string parse_property_name(std::istringstream & strm);
     static bool parse_property_value(std::istringstream & strm, std::string & result);
 public:
-    static std::string chop_from_file(std::string fname, int index);
-    static std::vector<std::string> chop_all(std::string fname);
-    static void parse(std::istringstream & strm, SGFTree * node);  
-    static int count_games_in_file(std::string filename);  
+    static std::string chop_from_file(std::string fname, size_t index);
+    static std::vector<std::string> chop_all(std::string fname,
+                                             size_t stopat = SIZE_MAX);
+    static void parse(std::istringstream & strm, SGFTree * node);
+    static int count_games_in_file(std::string filename);
 };
 
 
