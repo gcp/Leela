9589a83b8fa20525112e82ac61d071f15be00611
Gian-Carlo Pascutto <gcp@sjeng.org>
2017-03-24 19:07:37 +0100

    Let the compiler be the clever one.

--- Random.h
+++ Random.h
@@ -2,6 +2,9 @@
 #define RANDOM_H_INCLUDED
 
 #include "config.h"
+#include <type_traits>
+#include <stdint.h>
+
 
 /*
     Random number generator xoroshiro128+
@@ -18,7 +21,8 @@ public:
     */
     template<int MAX>
     uint32 randfix() {
-        return ((random() >> 48) * MAX) >> 16;
+        static_assert(0 < MAX && MAX < UINT32_MAX, "randfix out of range");
+        return random() % MAX;
     }
 
     uint32 randint16(const uint16 max);
