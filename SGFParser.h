f20313c77012c45e1bc91cb231bb892d6e189c85
Gian-Carlo Pascutto <gcp@sjeng.org>
2017-05-11 10:13:36 +0200

    Remove accidental duplicate class qualification.

--- SGFParser.h
+++ SGFParser.h
@@ -15,8 +15,8 @@ public:
     static std::string chop_from_file(std::string fname, size_t index);
     static std::vector<std::string> chop_all(std::string fname,
                                              size_t stopat = SIZE_MAX);
-    static std::vector<std::string> SGFParser::chop_stream(std::istream& ins,
-                                                           size_t stopat = SIZE_MAX);
+    static std::vector<std::string> chop_stream(std::istream& ins,
+                                                size_t stopat = SIZE_MAX);
     static void parse(std::istringstream & strm, SGFTree * node);
     static int count_games_in_file(std::string filename);
 };
